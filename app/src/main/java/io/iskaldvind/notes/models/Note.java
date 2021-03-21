0cc907f02f4f968ec36500e1ae918528ca16647d
iskaldvind <iskaldvind@gmail.com>
2021-03-21 23:28:39 +0300

    homework

--- app/src/main/java/io/iskaldvind/notes/models/Note.java
+++ app/src/main/java/io/iskaldvind/notes/models/Note.java
@@ -1,18 +1,25 @@
 package io.iskaldvind.notes.models;
 
-public class Note {
+import java.io.Serializable;
+
+public class Note implements Serializable {
     
     private String title;
     private String description;
     private String date;
+    private String photo;
+    private String url;
+    
     
     public Note(String title, String description, String date) {
         this.title = title;
         this.description = description;
         this.date = date;
+        this.photo = null;
+        this.url = null;
     }
-
-
+    
+    
     public String getTitle() {
         return title;
     }
@@ -25,5 +32,21 @@ public class Note {
  
     public String getDate() {
         return date;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
